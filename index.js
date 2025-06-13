5a90b0326fb71d1e30f43ad69d880a8809151169
andrew <andrew@kloudfuse.com>
2025-06-13 16:20:09 -0700

    KF_VIEW_TRACKER, ddTiming and <add|start|stop>DurationVital

--- index.js
+++ index.js
@@ -8,6 +8,59 @@ const SESSION_STORE_KEY = "_dd_s";
 const SESSION_ENTRY_REGEXP = /^([a-zA-Z]+)=([a-z0-9-]+)$/;
 const SESSION_ENTRY_SEPARATOR = "&";
 
+const COMMA_SEPARATED_KEY_VALUE = /([\w-]+)\s*=\s*([^;]+)/g;
+
+let KF_VIEW_TRACKER = {};
+let KF_SESSION_START = null;
+
+function findCommaSeparatedValue(rawString, name) {
+  COMMA_SEPARATED_KEY_VALUE.lastIndex = 0
+  // eslint-disable-next-line no-constant-condition
+  while (true) {
+    const match = COMMA_SEPARATED_KEY_VALUE.exec(rawString)
+    if (match) {
+      if (match[1] === name) {
+        return match[2]
+      }
+    } else {
+      break
+    }
+  }
+}
+
+function isValidSessionString(sessionString) {
+  return (
+    !!sessionString &&
+    (sessionString.indexOf(SESSION_ENTRY_SEPARATOR) !== -1 || SESSION_ENTRY_REGEXP.test(sessionString))
+  )
+}
+
+function toSessionState(sessionString) {
+  const session = {}
+  if (isValidSessionString(sessionString)) {
+    sessionString.split(SESSION_ENTRY_SEPARATOR).forEach((entry) => {
+      const matches = SESSION_ENTRY_REGEXP.exec(entry)
+      if (matches !== null) {
+        const [, key, value] = matches
+        if (key === 'aid') {
+          // we use `aid` as a key for anonymousId
+          session.anonymousId = value
+        } else {
+          session[key] = value
+        }
+      }
+    })
+  }
+  return session
+}
+
+
+const findSessionStart = () => {
+  let cookieValue = findCommaSeparatedValue(document.cookie, '_dd_s')
+  let sessionState = toSessionState(cookieValue)
+  return Number(sessionState.created)
+}
+
 const toSessionState = (sessionString) => {
   const session = {};
   if (isValidSessionString(sessionString)) {
@@ -53,6 +106,20 @@ const initDDBrowserSdk = ({ config, hasReplayBeenInitedRef, tabId }) => {
       if (hasReplayBeenInitedRef.current) {
         event.context.rrweb_has_replay = true;
       }
+
+      if (KF_SESSION_START == null) {
+        KF_SESSION_START = findSessionStart()
+      }
+
+      let viewId = null;
+      if (event.type == "view") {
+        viewId = event.id;
+        KF_VIEW_TRACKER[viewId] = event.date;
+      } else {
+        viewId = event.view.id;
+      }
+      event.context.kf_session_start_ms = KF_SESSION_START;
+      event.context.kf_view_start_ms = KF_VIEW_TRACKER[event.id];
 
       if (typeof config.beforeSend === "function") {
         return config.beforeSend(event);
@@ -143,6 +210,38 @@ class BrowserSdk {
       datadogRum.addError(error, context);
     } catch (e) {
       console.error('Failed to addError', e);
+    }
+  }
+
+  addTiming(...args) {
+    try {
+      datadogRum.addTiming(...args);
+    } catch (e) {
+      console.error('Failed to addTiming', e);
+    }
+  }
+
+  addDurationVital(...args) {
+    try {
+      datadogRum.addDurationVital(...args);
+    } catch (e) {
+      console.error('Failed to addDurationVital', e);
+    }
+  }
+
+  startDurationVital(...args) {
+    try {
+      datadogRum.startDurationVital(...args);
+    } catch (e) {
+      console.error('Failed to startDurationVital', e);
+    }
+  }
+
+  stopDurationVital(...args) {
+    try {
+      datadogRum.stopDurationVital(...args);
+    } catch (e) {
+      console.error('Failed to stopDurationVital', e);
     }
   }
 
